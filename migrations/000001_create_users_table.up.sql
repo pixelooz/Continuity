93bc1f51006fc296733594af498c5745d637eb09
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-03-02 10:06:15 +0530

    feat: updated users table schema with activation status and improved field definitions

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -2,10 +2,12 @@ CREATE EXTENSION IF NOT EXISTS citext;
 
 CREATE TABLE IF NOT EXISTS users
 (
-    id         UUID PRIMARY KEY,
-    username   VARCHAR(50) UNIQUE NOT NULL,
-    email      CITEXT UNIQUE      NOT NULL,
-    pass_hash  TEXT               NOT NULL,
-    created_at TIMESTAMPTZ        NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMPTZ        NOT NULL DEFAULT NOW()
+    id            UUID PRIMARY KEY,
+    name          VARCHAR(128)        NOT NULL,
+    username      VARCHAR(128) UNIQUE NOT NULL,
+    email         CITEXT UNIQUE       NOT NULL,
+    password_hash BYTEA               NOT NULL,
+    activated     BOOL                NOT NULL DEFAULT FALSE,
+    created_at    TIMESTAMPTZ         NOT NULL DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ         NOT NULL DEFAULT NOW()
 );
