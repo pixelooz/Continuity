abb6a236b2cf7272a7ce746cbc6fd1c6b9d11db4
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-03-02 20:52:18 +0530

    feat: add token model, validation utilities, and password hashing methods

--- internal/validate/users.go
+++ internal/validate/users.go
@@ -1,1 +1,47 @@
 package validate
+
+import (
+	"Continuity/internal/data"
+	"regexp"
+)
+
+var EmailRegex = regexp.MustCompile(
+	"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9]" +
+		"(?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?" +
+		"(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
+)
+
+// ValidateEmail checks if the given email is valid.
+func ValidateEmail(v *Validator, email string) {
+	v.CheckField(NotBlank(email), "email", "must not be blank")
+	v.CheckField(Matches(email, EmailRegex), "email", "invalid email address")
+}
+
+// ValidatePlainPassword checks if the given plaintext password is valid.
+func ValidatePlainPassword(v *Validator, password string) {
+	v.CheckField(NotBlank(password), "password", "must not be blank")
+	v.CheckField(MinChars(password, 8), "password", "must be greater than 8 characters")
+	v.CheckField(MaxChars(password, 72), "password", "must be lesser than 72 characters")
+}
+
+// ValidateUser checks if the given user is valid.
+func ValidateUser(v *Validator, u *data.User) {
+	v.CheckField(NotBlank(u.Name), "name", "must not be blank")
+	v.CheckField(MaxChars(u.Name, 128), "name", "must be lesser than 128 characters")
+
+	v.CheckField(NotBlank(u.Username), "username", "must not be blank")
+	v.CheckField(MaxChars(u.Username, 128), "username", "must be lesser than 128 characters")
+
+	ValidateEmail(v, u.Email)
+	ValidatePlainPassword(v, u.Password.PlainText)
+
+	if u.Password.Hash == nil {
+		panic("missing password hash for user")
+	}
+}
+
+// ValidatePlainToken checks if the given plaintext token is valid.
+func ValidatePlainToken(v *Validator, plainToken string) {
+	v.Check(NotBlank(plainToken), "plainToken", "must not be blank")
+	v.Check(len(plainToken) == 26, "plainToken", "must be 26 characters long")
+}
--- internal/validate/validate.go
+++ internal/validate/validate.go
@@ -1,1 +1,74 @@
 package validate
+
+import (
+	"regexp"
+	"strings"
+	"unicode/utf8"
+)
+
+// Validator holds validation error values in key/value pairs.
+type Validator struct {
+	FieldErrors map[string]string
+	Errors      map[string]string
+}
+
+// NewValidator returns a new initialized Validator struct.
+func NewValidator() *Validator {
+	return &Validator{
+		Errors:      make(map[string]string),
+		FieldErrors: make(map[string]string),
+	}
+}
+
+// Valid returns true if there are no errors.
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0 && len(v.FieldErrors) == 0
+}
+
+// AddFieldError adds a new error to the FieldErrors map.
+func (v *Validator) AddFieldError(key, value string) {
+	if _, exists := v.FieldErrors[key]; !exists {
+		v.FieldErrors[key] = value
+	}
+}
+
+// AddError adds a new error to the Errors map.
+func (v *Validator) AddError(key, value string) {
+	if _, exists := v.Errors[key]; !exists {
+		v.FieldErrors[key] = value
+	}
+}
+
+// Check adds a new error to the Errors map if the given condition is false.
+func (v *Validator) Check(valid bool, key, value string) {
+	if !valid {
+		v.AddError(key, value)
+	}
+}
+
+// CheckField adds a new error to the FieldErrors map if the given condition is false.
+func (v *Validator) CheckField(valid bool, key, value string) {
+	if !valid {
+		v.AddFieldError(key, value)
+	}
+}
+
+// NotBlank returns true if the given string is not empty after trimming whitespace.
+func NotBlank(str string) bool {
+	return strings.TrimSpace(str) != ""
+}
+
+// Matches returns true if the given string matches the given regular expression.
+func Matches(str string, rx *regexp.Regexp) bool {
+	return rx.MatchString(str)
+}
+
+// MaxChars returns true if the provided string is shorter than the given size.
+func MaxChars(str string, size int) bool {
+	return utf8.RuneCountInString(str) <= size
+}
+
+// MinChars returns true if the provided string is longer than the given size.
+func MinChars(str string, size int) bool {
+	return utf8.RuneCountInString(str) >= size
+}
