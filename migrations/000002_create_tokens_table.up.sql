e6d920e4ae046faf38c4b09ab2e1bf770b54a869
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-03-02 20:19:19 +0530

    add: migrations for token table

---
+++ migrations/000002_create_tokens_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS tokens
+(
+    user_id    UUID        NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    hash       bytea       NOT NULL,
+    scope      TEXT        NOT NULL,
+    expires_at TIMESTAMPTZ NOT NULL
+);
