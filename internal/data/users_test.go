2171b403e349a963265863099c7948a40d32fc2b
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-03-03 01:09:39 +0530

    test: small test for helper function

---
+++ internal/data/users_test.go
@@ -0,0 +1,34 @@
+package data
+
+import (
+	"testing"
+
+	"github.com/lib/pq"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMapUserInsertErr(t *testing.T) {
+	pqErr := &pq.Error{
+		Code:       "23505",
+		Constraint: "users_username_key",
+	}
+	err := mapUserInsertErr(pqErr)
+	require.Equal(t, err, ErrDuplicateUsername)
+	require.NotEqual(t, err, ErrDuplicateEmail)
+
+	pqErr = &pq.Error{
+		Code:       "23505",
+		Constraint: "users_email_key",
+	}
+	err = mapUserInsertErr(pqErr)
+	require.Equal(t, err, ErrDuplicateEmail)
+	require.NotEqual(t, err, ErrDuplicateUsername)
+
+	pqErr = &pq.Error{
+		Code:       "22001",
+		Constraint: "character too long",
+	}
+	err = mapUserInsertErr(pqErr)
+	require.NotEqual(t, err, ErrDuplicateEmail)
+	require.NotEqual(t, err, ErrDuplicateUsername)
+}
