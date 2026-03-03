4ba896263c7d3cd96cb8f1bdce337e18ceb2170f
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-03-03 12:12:49 +0530

    add: templateData struct and helper for passing common data to templates

--- cmd/web/helper.go
+++ cmd/web/helper.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+func (b *backend) newTemplateData(c echo.Context) *templateData {
+	return &templateData{
+		CreatedAt: time.Now().Format("03 Jan 2006"),
+		CSRFToken: c.Get("csrf").(string),
+		// UserView: b.currentUserView(c),
+		// IsAuthenticated: b.isAuthenticated(c),
+	}
+}
