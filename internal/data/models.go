bef96ef04182e60e31701ac26b4f1a194b4565be
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-03-03 09:36:32 +0530

    add: add TokenModel to Models and update dependencies

--- internal/data/models.go
+++ internal/data/models.go
@@ -3,13 +3,17 @@ package data
 import "database/sql"
 
 type Models struct {
-	Users UserModel
+	Users  UserModel
+	Tokens TokenModel
 }
 
 // NewModels returns a new Models struct with all the models wired to the provided db connection.
 func NewModels(db *sql.DB) *Models {
 	return &Models{
 		Users: UserModel{
+			DB: db,
+		},
+		Tokens: TokenModel{
 			DB: db,
 		},
 	}
